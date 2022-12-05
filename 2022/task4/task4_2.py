457336270c3bac92f88992b80dd1615c78b19d55
pawel.kajanek <pawel.kajanek@stxnext.pl>
2022-12-05 10:18:52 +0100

    2022 - Day 4

---
+++ 2022/task4/task4_2.py
@@ -0,0 +1,16 @@
+f = open('./input.txt', 'r')
+pairs = f.readlines()
+f.close()
+
+count = 0
+
+for pair in pairs:
+    pair = pair[:-1]
+    e1, e2 = pair.split(',')
+    e1_start, e1_end = e1.split('-')
+    e2_start, e2_end = e2.split('-')
+    if ((int(e1_start) <= int(e2_end) and int(e1_end) >= int(e2_start)) 
+        or (int(e2_start) <= int(e1_end) and int(e2_end) >= int(e1_start))):
+        count += 1
+
+print(count)
